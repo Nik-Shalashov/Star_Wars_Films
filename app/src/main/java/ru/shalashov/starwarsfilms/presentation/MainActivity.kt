82df7c82a84cad9689ecb7cfc1195fb3c893238d
Nik-Shalashov <monitor1@inbox.ru>
2022-05-18 01:25:19 +0700

    Initial commit

---
+++ app/src/main/java/ru/shalashov/starwarsfilms/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.shalashov.starwarsfilms.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.shalashov.starwarsfilms.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
